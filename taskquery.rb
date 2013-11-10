62fcd001136dbc5b11e7a33d11d50db2cfa1a398
Jeroen Budts <jeroen@inuits.eu>
2013-11-10 13:50:52 +0100

    initial version of the script

---
+++ taskquery.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+DEFAULT_REPORT = 'all'
+
+command = ARGV.join(' ')
+
+def get_key_from_cmd(key, cmd)
+  regex = /#{key}:(\S+)/
+  match = regex.match(cmd)
+  if match
+    return cmd.sub(regex, ''), match[1]
+  end
+  return cmd, nil
+end
+
+command, columns = get_key_from_cmd('columns', command)
+command, sort = get_key_from_cmd('sort', command)
+command, labels = get_key_from_cmd('labels', command)
+command, report = get_key_from_cmd('report', command)
+
+report = DEFAULT_REPORT if report.nil?
+
+new_cmd = "rc.report.#{report}.columns=#{columns} " unless columns.nil?
+new_cmd += "rc.report.#{report}.labels=#{labels} " unless labels.nil?
+if labels.nil? and !columns.nil?
+  new_cmd += "rc.report.#{report}.labels=#{columns} "
+end
+new_cmd += "rc.report.#{report}.sort=#{sort} " unless sort.nil?
+new_cmd += " #{command} #{report}"
+
+puts new_cmd
